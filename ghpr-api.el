28e18f7ecb05f20e722ab348ad25dd74b3403868
Lucas Sta Maria <lucas@priime.dev>
2025-08-22 20:30:15 +0800

    chore: create base project

---
+++ ghpr-api.el
@@ -0,0 +1,28 @@
+;;; ghpr-api.el --- Interactions for the GitHub API -*- lexical-binding: t -*-
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Provides procedures for interacting with the GitHub API, including
+;; authentication.
+
+;;; Code:
+
+(provide 'ghpr-api)
+
+;;; ghpr-api.el ends here
---
+++ ghpr-review.el
@@ -0,0 +1,36 @@
+;;; ghpr-review.el --- PR workflow functionality -*- lexical-binding: t -*-
+
+;; Author: Lucas Sta Maria
+;; Maintainer: Lucas Sta Maria
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: homepage
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; ghpr-review.el provides the functionality for interacting with a pull request
+;; -- from inserting PR comments to viewing the PR diffs.
+
+;;; Code:
+
+(provide 'ghpr-review)
+
+;;; ghpr-review.el ends here
